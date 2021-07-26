420b9edbdca51f50f7600ddf25495944f293bca0
Michelle Xie <mxie220@aucklanduni.ac.nz>
2021-07-26 13:59:39 +1200

    Added state saving and loading.
    Using pickle and bz2.

--- exercise-1-find-nounce.py
+++ exercise-1-find-nounce.py
@@ -3,7 +3,8 @@ Exercise 1: Finding the nounce.
 Author: Michelle Xie
 '''
 
-import hashlib, string, datetime
+import hashlib, string, datetime, pickle, os, bz2
+from typing import Type
 
 def build_nounce(start_list, alphanumeric, nounce_length):
     output_list = []
@@ -29,19 +30,46 @@ def try_hash(input_string, nounce):
     if hash_result[0:4] == '0000':
         print("Nounce found: {0}".format(nounce))
         return True
-    print("False: {0}, search continues.".format(nounce))
+    print("False: {0}. Search continues. Press CTRL + C for options.".format(nounce))
     return False
             
 def find_nounce(input_string, alphanumeric, length):
-    nounce_length = length - len(input_string)
-    nounce_list = build_nounce([], alphanumeric, nounce_length)
-    while len(nounce_list) > 0:
-        nounce = nounce_list.pop(0)
-        if try_hash(input_string, nounce):
-            return nounce
+    try:
+        nounce_length = length - len(input_string)
+        filename = os.getcwd() + '/' + input_string + '.bz2'
+        if os.path.exists(filename):
+            print("Previous state has been found for this input. Would you like to resume?")
+            print("Y = yes")
+            print("N = no")
+            resume = input()
+            if resume == "Y" or resume == "yes" or resume == "Yes" or resume == "YES" or resume == "y":
+                input_file = bz2.BZ2File(filename, 'rb')
+                nounce_list = pickle.load(input_file)
+                input_file.close()
+                print(type(nounce_list))
+            else:
+                nounce_list = build_nounce([], alphanumeric, nounce_length)
+            os.remove(filename)
         else:
-            nounce_list += build_nounce([nounce], alphanumeric, nounce_length)
+            nounce_list = build_nounce([], alphanumeric, nounce_length)
 
+        while len(nounce_list) > 0:
+            nounce = nounce_list.pop(0)
+            if try_hash(input_string, nounce):
+                return nounce
+            else:
+                nounce_list += build_nounce([nounce], alphanumeric, nounce_length)
+
+    except KeyboardInterrupt:
+        print("Do you want to save the current state?")
+        print("Y = yes")
+        print("N = no")
+        state = input()
+        if state == "Y" or state == "y" or state == "yes" or state == "Yes" or state == "YES":
+            output_file = bz2.BZ2File(filename, 'wb')
+            pickle.dump(nounce_list, output_file)
+            output_file.close()
+            print("State saved to {0}".format(filename))
 
 def main():
     alphanumeric = string.ascii_letters + string.digits
